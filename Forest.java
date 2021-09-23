62b4f74fa535411508e54a3fad0fbbd47de5a80c
poojaprakas <89781163+poojaprakas@users.noreply.github.com>
2021-09-23 00:33:07 -0700

    Add files via upload

---
+++ Forest.java
@@ -0,0 +1,23 @@
+package com.xworkz.oopConcepts;
+
+public class Forest {
+	String structure;
+	int temperature;
+	int density;
+	String texture;
+	int aeration;
+	
+
+	public void habitat() {
+		System.out.println("give shelter to the animals and humans");
+
+	}
+	public void soilprotection() {
+		System.out.println("trees prevent the removal of soil");
+		
+	}
+	public void economicpurposes() {
+		System.out.println("it is ecofriendly and used for economic purpose");
+	}
+
+}
